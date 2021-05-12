61596a1e1676417aab59d8879e5dc4ef4b01435f
ConcoleFirst <alek.kalashnikov32@yandex.ru>
2021-05-12 17:36:11 +0300

    formula

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -1,17 +1,19 @@
 package com.company;
 
 public class Main2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int a1 = 4;
-        int b1 = 7;
+        int a = 4;
+        int b = 7;
 
-        System.out.println("a = " + a1 + " b = " + b1);
+        System.out.println("a = " + a + " b = " + b);
 
-        int a2 = (a1 + b1) - a1;
-        int b2 = (a1 + b1) - b1;
+        a = a + b;
+        b = a - b;
+        a -= b;
 
-        System.out.println("a = " + a2 + " b = " + b2);
+
+        System.out.println("a = " + a + " b = " + b);
 
     }
 
